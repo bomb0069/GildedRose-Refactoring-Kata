8a5dfaff06e5c45b5ae3a3af49d13766bcaa579b
karan sivarat <bomb0069@gmail.com>
2020-01-17 21:50:43 +0700

    [Added]: Test normal_item_will_decrease_quality_and_sellIn_when_the_day_pass

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void normal_item_will_decrease_quality_and_sellIn_when_the_day_pass() {
+        Item[] items = new Item[] { new Item("Normal Item", 1, 1) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals(0, app.items[0].sellIn);
+        assertEquals(0, app.items[0].quality);
     }
 
 }
