95c91fa77249ab1e044e6992bbd0a38181f108c0
karan sivarat <bomb0069@gmail.com>
2020-01-18 00:35:46 +0700

    [Refactor]: by extract isPassSellByDate and rearrange condition by move category to the first level

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -12,7 +12,6 @@ class GildedRose {
             if (isAgedBrie(item)) {
                 if (isQualityLessThanMaximum(item))
                     item.quality = item.quality + 1;
-
             } else if (isBackstagePasses(item)) {
                 if (isQualityLessThanMaximum(item)) {
                     item.quality = item.quality + 1;
@@ -22,7 +21,7 @@ class GildedRose {
                             item.quality = item.quality + 1;
                         }
                     }
-
+                    
                     if (item.sellIn < 6) {
                         if (isQualityLessThanMaximum(item)) {
                             item.quality = item.quality + 1;
@@ -36,29 +35,32 @@ class GildedRose {
                     }
                 }
             }
-
+            
             if (!isSulfuras(item)) {
                 item.sellIn = item.sellIn - 1;
             }
-
-            if (item.sellIn < 0) {
-                if (isAgedBrie(item)) {
+            
+            if (isAgedBrie(item)) {
+                if (isPassedSellByDate(item))
                     if (isQualityLessThanMaximum(item)) {
                         item.quality = item.quality + 1;
                     }
-                } else {
-                    if (!isBackstagePasses(item)) {
-                        if (item.quality > 0) {
-                            if (!isSulfuras(item)) {
-                                item.quality = item.quality - 1;
-                            }
+            } else if (isBackstagePasses(item)) {
+                if (isPassedSellByDate(item))
+                    item.quality = item.quality - item.quality;
+            } else {
+                if (isPassedSellByDate(item))
+                    if (item.quality > 0) {
+                        if (!isSulfuras(item)) {
+                            item.quality = item.quality - 1;
                         }
-                    } else {
-                        item.quality = item.quality - item.quality;
                     }
-                }
             }
         }
+    }
+
+    private boolean isPassedSellByDate(Item item) {
+        return item.sellIn < 0;
     }
 
     private boolean isQualityLessThanMaximum(Item item) {
