eccd04477fc7142be4c001bd02c9505df6005615
karan sivarat <bomb0069@gmail.com>
2020-01-17 23:01:03 +0700

    [Refactor]: extract assetion to method assertItemWithSellInDaysAndQuality

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -11,26 +11,28 @@ class GildedRoseTest {
         Item[] items = new Item[] { new Item("Normal Item", 1, 1) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals(0, app.items[0].sellIn);
-        assertEquals(0, app.items[0].quality);
+        assertItemWithSellInDaysAndQuality(0, 0, app.items[0]);
     }
-
+    
     @Test
     void the_quality_of_an_item_is_never_negative() {
         Item[] items = new Item[] { new Item("Normal Item", 2, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals(1, app.items[0].sellIn);
-        assertEquals(0, app.items[0].quality);
+        assertItemWithSellInDaysAndQuality(1, 0, app.items[0]);
     }
-
+    
     @Test
     void once_the_sell_by_date_has_passed_quality_degrades_twice_as_fast() {
         Item[] items = new Item[] { new Item("Normal Item", 0, 8) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals(-1, app.items[0].sellIn);
-        assertEquals(6, app.items[0].quality);
+        assertItemWithSellInDaysAndQuality(-1, 6, app.items[0]);
     }
 
+    private void assertItemWithSellInDaysAndQuality(int expectedSellIn, int expectedQuality, Item item) {
+        assertEquals(expectedSellIn, item.sellIn);
+        assertEquals(expectedQuality, item.quality);
+    }
+    
 }
